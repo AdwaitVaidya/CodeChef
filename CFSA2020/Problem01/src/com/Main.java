a134dd3eedc6133995c410edb216673fc5457ee0
Adwait Vaidya <adwait.vaidya120@gmail.com>
2020-09-19 17:43:14 +0530

    commit Code-o-Fiesta

---
+++ CFSA2020/Problem01/src/com/Main.java
@@ -0,0 +1,41 @@
+package com;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	    String empty = "";
+        int counter = 0;
+        StringTokenizer stringTokenizer = null;
+        try {
+            stringTokenizer = new StringTokenizer(input.readLine(), " ");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (stringTokenizer.hasMoreTokens()){
+            counter++;
+            String string = stringTokenizer.nextToken();
+            if (string.charAt(0) == 'a' || string.charAt(0) == 'e' || string.charAt(0) == 'i' || string.charAt(0) == 'o' || string.charAt(0) == 'u' ){
+                string = string + "d";
+            }
+            else {
+                char c = string.charAt(0);
+                string = string.substring(1);
+                string = string + c + "d";
+            }
+            for (int i = 0; i < counter; i++) {
+                string += "u";
+            }
+            empty= empty + string + " ";
+        }
+
+        System.out.println(empty);
+
+
+    }
+}
---
+++ CFSA2020/Program02/src/com/Main.java
@@ -0,0 +1,52 @@
+package com;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static final String rs = "Rs. ";
+
+    public static void main(String[] args) {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = null;
+        try {
+            stringTokenizer = new StringTokenizer(input.readLine(), " ");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int breadSlices = Integer.parseInt(stringTokenizer.nextToken());
+        int cheeseSlices = Integer.parseInt(stringTokenizer.nextToken());
+        boolean possible = false;
+int maxIncome = 0;
+int maxx = 0;
+int maxy = 0;
+        for (int x = 0; x <= breadSlices; x++) {
+            int y = (breadSlices/2) - x;
+            if (((2*x)+y) == cheeseSlices && y >= 0){
+                possible = true;
+                int income = (x*220) + (y*120);
+                if (income > maxIncome){
+                    maxIncome = income;
+                    maxx = x;
+                    maxy = y;
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+        if (possible){
+            System.out.println(maxx+" "+maxy);
+
+            System.out.println(rs + maxIncome);
+        }
+        else {
+            System.out.println("-1");
+        }
+
+    }
+
+}
